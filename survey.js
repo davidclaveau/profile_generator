3760ead25e62955406a68f7b40b90dedb58a803f
David Claveau <daveclaveau@gmail.com>
2021-05-04 21:33:01 +0000

    Added survey.js which askes several questions using Node and outputs a paragraph of the answers.

---
+++ survey.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const questions = [
+  "What's your name? Nicknames are also acceptable :) ",
+  "What's an activity you like doing? ",
+  "What do you listen to while doing that? ",
+  "Which meal is your favourite (eg: dinner, brunch, etc.) ",
+  "What's your favourite thing to eat for that meal? ",
+  "Which sport is your absolute favourite? ",
+  "What is your superpower? In a few words, tell us what you are amazing at! ",
+];
+
+const answers = [];
+let num = 0;
+
+const ask = (num) => {
+  rl.question(questions[num], (answer) => {
+    answers.push(answer);
+    if (num < questions.length - 1) {
+      ask((num + 1));
+    } else {
+      rl.close();
+
+      console.log(`${answers[0]} loves listening to ${answers[2]} while ${answers[1]}, devouring ${answers[4]} for ${answers[3]}, prefers ${answers[5]} over any other sport, and is amazing at ${answers[6]}.`);
+    }
+  });
+};
+
+ask(num);
